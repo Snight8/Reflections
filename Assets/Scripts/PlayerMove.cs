524b8f3f9a35dab9b74835b8a1106f9a632fc513
Chesyon <55322011+Chesyon@users.noreply.github.com>
2024-02-06 16:45:42 -0500

    Added PlayerMove script
    very simple for now, definitely going to make it not awful later

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Vector3 input;
+    public Vector3 movement;
+    public float speed;
+    public Transform player;
+    public Transform playerMirror;
+    // Update is called once per frame
+    void Update()
+    {
+        input = new(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        //end meth calculation
+        player.Translate(MovementCalc(input));
+        playerMirror.Translate(MovementCalc(input, true));
+    }
+    Vector3 MovementCalc(Vector3 input, bool flipX = false, bool flipZ = false)
+    {
+        if (flipX) input.x = -input.x;
+        if (flipZ) input.z = -input.z;
+        Vector3 movement = new(input.x * speed, 0, input.z * speed);
+        return movement;
+    }
+}
+
+
