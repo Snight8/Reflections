2d80244cc8f49674e7410d81dbf00aded26aa0ed
Snight <159181379+Snight8@users.noreply.github.com>
2024-04-04 12:50:43 -0400

    new stuff
    idk

---
+++ Assets/Scripts/DoorTypes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTypes : MonoBehaviour
+{
+
+    public bool PlayerDoor;
+    public bool MirrorDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MirrorPlayer" && MirrorDoor == true)
+        {
+            Destroy(gameObject);
+        }
+        if (other.gameObject.tag == "Player" && PlayerDoor == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
