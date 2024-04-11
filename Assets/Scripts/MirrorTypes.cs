99c0a772a3af1283e12f6d9ed91737491d80f21f
Snight <159181379+Snight8@users.noreply.github.com>
2024-04-11 12:49:39 -0400

    im working on it
    idk man

--- Assets/Scripts/MirrorTypes.cs
+++ Assets/Scripts/MirrorTypes.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class MirrorTypes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera realCamera;
+    public bool xMirrorLeft;
+    public bool xMirrorRight;
+    public bool zMirrorBottom;
+    public bool zMirrorTop;
+
+    void OnCollisionEnter(Collision collision)
     {
-        
+        StartCoroutine(MirrorDoesThings());
+
+        if (xMirrorLeft == true)
+        {
+            
+        }
+
+        if (xMirrorRight == true)
+        {
+           
+        }
+
+        if (zMirrorBottom == true)
+        {
+           
+        }
+
+        if (zMirrorTop == true)
+        {
+           
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MirrorDoesThings()
     {
-        
+        //camera has a color flash and a sound plays
+        yield return null;
     }
 }
