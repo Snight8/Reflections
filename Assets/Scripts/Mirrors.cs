ca1f7cf3cd3898a4b10f42f07cff800347682e14
Snight <159181379+Snight8@users.noreply.github.com>
2024-04-22 09:40:13 -0400

    dude im messing up this code so bad
    idk wtf im doing im sorry :<

---
+++ Assets/Scripts/Mirrors.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirrors : MonoBehaviour
+{
+    public Camera realCamera;
+    bool yourColliderDied = false;
+    GameObject hesHere;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "MirrorPlayer")
+        {
+            Debug.Log("You're in.");
+            yourColliderDied = true;
+            hesHere = other.gameObject;
+            hesHere.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+            hesHere.GetComponent<Collider>().enabled = false;
+            StartCoroutine(MirrorDoesThings());
+            StartCoroutine(GetOutPlease());
+        }
+    }
+
+    private void OnCollisonExit(Collision other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "MirrorPlayer")
+        {
+            Debug.Log("You're out.");
+        }
+    }
+
+    IEnumerator MirrorDoesThings()
+    {
+        //camera has a color flash and a sound plays
+        yourColliderDied = false;
+        yield return null;
+    }
+
+    IEnumerator GetOutPlease()
+    {
+        yield return new WaitForSeconds(5);
+        hesHere.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        hesHere.GetComponent<Collider>().enabled = true;
+        yield return null;
+    }
+}
