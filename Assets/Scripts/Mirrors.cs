389960c2ba0746a90697a97e770c789cb46b404b
Snight <159181379+Snight8@users.noreply.github.com>
2024-04-24 08:24:55 -0400

    it's janky but it works!
    i did it! yupi!
    
    mirrors now allow you to go through them + other player.
    light effects in bg

--- Assets/Scripts/Mirrors.cs
+++ Assets/Scripts/Mirrors.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class Mirrors : MonoBehaviour
 {
     public Camera realCamera;
-    bool yourColliderDied = false;
     GameObject hesHere;
+    GameObject twoOfThem;
+    public bool realPlayerMirror;
+    public bool mirrorPlayerMirror;
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" || other.gameObject.tag == "MirrorPlayer")
         {
+            if(realPlayerMirror == true && other.gameObject.tag == "Player")
+            {
+                hesHere = other.gameObject;
+                constraintsForGameObject(1);
+                hesHere.GetComponent<Collider>().enabled = false;
+            }
+            else if (mirrorPlayerMirror == true && other.gameObject.tag == "MirrorPlayer")
+            {
+                twoOfThem = other.gameObject;
+                constraintsForGameObject(2);
+                twoOfThem.GetComponent<Collider>().enabled = false;
+            }
             Debug.Log("You're in.");
-            yourColliderDied = true;
-            hesHere = other.gameObject;
-            hesHere.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
-            hesHere.GetComponent<Collider>().enabled = false;
             StartCoroutine(MirrorDoesThings());
             StartCoroutine(GetOutPlease());
         }
     }
 
-    private void OnCollisonExit(Collision other)
+    private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player" || other.gameObject.tag == "MirrorPlayer")
         {
@@ -30,18 +40,46 @@ public class Mirrors : MonoBehaviour
         }
     }
 
+    void constraintsForGameObject(int typeOfConstraints)
+    {
+        if (typeOfConstraints == 1)
+        {
+            hesHere.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+        }
+
+        if (typeOfConstraints == 2)
+        {
+            twoOfThem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+        }
+    }
+
     IEnumerator MirrorDoesThings()
     {
-        //camera has a color flash and a sound plays
-        yourColliderDied = false;
+        realCamera.clearFlags = CameraClearFlags.Color;
+        yield return new WaitForSeconds(.1f);
+        realCamera.backgroundColor = Color.blue;
+        yield return new WaitForSeconds(.1f);
+        realCamera.backgroundColor = Color.white;
+        yield return new WaitForSeconds(.1f);
+        realCamera.backgroundColor = Color.cyan;
+        yield return new WaitForSeconds(.1f);
+        realCamera.clearFlags = CameraClearFlags.Skybox;
         yield return null;
     }
 
     IEnumerator GetOutPlease()
     {
-        yield return new WaitForSeconds(5);
-        hesHere.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-        hesHere.GetComponent<Collider>().enabled = true;
+        yield return new WaitForSeconds(1);
+        if (realPlayerMirror == true)
+        {
+            hesHere.GetComponent<Collider>().enabled = true;
+            hesHere.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+        else if (mirrorPlayerMirror == true)
+        {
+            twoOfThem.GetComponent<Collider>().enabled = true;
+            twoOfThem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
         yield return null;
     }
 }
