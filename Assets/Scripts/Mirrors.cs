f6243440f9e5645a9872b6fe049e8cee05b7475f
Snight <159181379+Snight8@users.noreply.github.com>
2024-04-25 12:50:03 -0400

    problems happening with new color thing
    aw shucks

--- Assets/Scripts/Mirrors.cs
+++ Assets/Scripts/Mirrors.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mirrors : MonoBehaviour
 {
-    public Camera realCamera; // main camera. not sure if this plays nice with cinemachine yet? need to test if flashes works while cameras are split
+    public Image flashAColor; //it's that png you told me to have instead of camera for effects
     GameObject hesHere; // standard player
     GameObject twoOfThem; // mirror player
     public bool realPlayerMirror; // is the mirror this script is assigned to for the real player?
@@ -34,10 +35,8 @@ public class Mirrors : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Player" || other.gameObject.tag == "MirrorPlayer")
-        {
             Debug.Log("You're out.");
-        }
+            flashAColor.color = new Color32(0, 0, 0, 0);
     }
 
     void constraintsForGameObject(int typeOfConstraints)
@@ -55,15 +54,14 @@ public class Mirrors : MonoBehaviour
 
     IEnumerator MirrorDoesThings()
     {
-        realCamera.clearFlags = CameraClearFlags.Color;
+        flashAColor.color = new Color32(255,255,255,100); //white
         yield return new WaitForSeconds(.1f);
-        realCamera.backgroundColor = Color.blue;
+        flashAColor.color = new Color32(96, 237, 247, 100); //cyan
         yield return new WaitForSeconds(.1f);
-        realCamera.backgroundColor = Color.white;
+        flashAColor.color = new Color32(12, 42, 194, 100); //blue
         yield return new WaitForSeconds(.1f);
-        realCamera.backgroundColor = Color.cyan;
+        flashAColor.color = new Color32(76, 125, 199, 100); //light blue
         yield return new WaitForSeconds(.1f);
-        realCamera.clearFlags = CameraClearFlags.Skybox;
         yield return null;
     }
 
---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public GameObject thisWillGoAway; //this is just any object that we want to appear/disappear if you have something on the plate
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //this.transform.position  //make pressure plate move down by 1 on the y axis
+        thisWillGoAway.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        thisWillGoAway.SetActive(true);
+    }
+}
