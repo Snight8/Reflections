a162bb84fb65633282d63e0e051abe55783a9a58
Snight <159181379+Snight8@users.noreply.github.com>
2024-05-02 12:49:45 -0400

    minor mirror fix and start pressure plate demo

--- Assets/Scripts/Mirrors.cs
+++ Assets/Scripts/Mirrors.cs
@@ -10,6 +10,7 @@ public class Mirrors : MonoBehaviour
     GameObject twoOfThem; // mirror player
     public bool realPlayerMirror; // is the mirror this script is assigned to for the real player?
     public bool mirrorPlayerMirror; //  is the mirror this script is assigned to for the mirror player?
+    public AudioClip passingThrough; //the sound that plays when you're in the mirror. Get it?!??!/1!?
 
     void OnTriggerEnter(Collider other)
     {
@@ -48,6 +49,7 @@ public class Mirrors : MonoBehaviour
 
     IEnumerator MirrorDoesThings()
     {
+        AudioSource.PlayClipAtPoint(passingThrough, transform.position);
         flashAColor.color = new Color32(255,255,255,100); //white
         yield return new WaitForSeconds(.05f);
         flashAColor.color = new Color32(96, 237, 247, 100); //cyan
@@ -62,12 +64,12 @@ public class Mirrors : MonoBehaviour
     IEnumerator GetOutPlease()
     {
         yield return new WaitForSeconds(0.25f); //the total of all the Waits in MirrorDoesThings need to be less than this wait
-        if (realPlayerMirror == true)
+        if (realPlayerMirror == true && hesHere.gameObject.tag == "Player")
         {
             hesHere.GetComponent<Collider>().enabled = true;
             hesHere.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
         }
-        else if (mirrorPlayerMirror == true)
+        if (mirrorPlayerMirror == true && twoOfThem.gameObject.tag == "MirrorPlayer")
         {
             twoOfThem.GetComponent<Collider>().enabled = true;
             twoOfThem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
