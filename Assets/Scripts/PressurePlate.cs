cb48b5b868f28c0bbdbc5e3f9c0cafbdaddf0894
Snight <159181379+Snight8@users.noreply.github.com>
2024-05-13 08:56:58 -0400

    did audio pressure plate

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+
+    public AudioClip plateSound;
     public GameObject thisWillGoAway; //this is just any object that we want to appear/disappear if you have something on the plate
 
     private void OnTriggerEnter(Collider other)
     {
         //this.transform.position   //make pressure plate move down by 1 on the y axis (idk how)
         thisWillGoAway.SetActive(false);
+        AudioSource.PlayClipAtPoint(plateSound, transform.position);
     }
 
     private void OnTriggerExit(Collider other)
     {
         thisWillGoAway.SetActive(true);
+        AudioSource.PlayClipAtPoint(plateSound, transform.position);
     }
 }
