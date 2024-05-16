f1303168a2bfa75534b99a24585c005ed239a37b
Chesyon <55322011+Chesyon@users.noreply.github.com>
2024-05-16 11:22:43 -0400

    Update MovingPlatform.cs
    implemented moving platforms

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     Vector3 dir;
-    float speed;
+    public float speed;
+    public PlatformHadKids targeting;
+    int previousTargetIndex;
+    int currentTargetIndex;
+    public Vector3[] targets;
+    float DistanceToNextTarget;
 
-    private void Start()
+    void Start() //do not use void Awake here!! Awake is called on load, and it's possible that mirrored platforms won't load on the same frame, which will desync them!
     {
-        //PlatformHadKids.movementDir([]);  //use this thing somewhere else. idk how to write this code
+         transform.position = targets[0]; //ensuring that we start at the first target
+         DistanceToNextTarget = Vector3.Distance(targets[0], targets[1]);
+         dir = targeting.movementDir(targets[1]);
     }
+    
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, targets[previousTargetIndex]) >= DistanceToNextTarget) //checks for overshoot. if this condition is true, it's time to switch targets!
+        {
+             transform.position = targets[currentTargetIndex]; // snap to target position in case of overshoot
+             previousTargetIndex = currentTargetIndex;
+             if(currentTargetIndex == targets.Length) currentTargetIndex = 0; // if at the end of target array, loop back to 0
+             else currentTargetIndex++; //otherwise, increment the index by 1
+             DistanceToNextTarget = Vector3.Distance(targets[previousTargetIndex], targets[currentTargetIndex]);
+             dir = targeting.movementDir(targets[currentTargetIndex]);
+        }
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+    
 }
