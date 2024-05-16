5eeffbd1288b3237d6b67eff7079607e09a42fb2
Snight <159181379+Snight8@users.noreply.github.com>
2024-05-16 12:23:49 -0400

    it working but you make better

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -8,29 +8,50 @@ public class MovingPlatform : MonoBehaviour
     public float speed;
     public PlatformHadKids targeting;
     int previousTargetIndex;
-    int currentTargetIndex;
+    public int currentTargetIndex;
     public Vector3[] targets;
     float DistanceToNextTarget;
+    public bool moveIfTouched;
 
     void Start() //do not use void Awake here!! Awake is called on load, and it's possible that mirrored platforms won't load on the same frame, which will desync them!
     {
          transform.position = targets[0]; //ensuring that we start at the first target
          DistanceToNextTarget = Vector3.Distance(targets[0], targets[1]);
          dir = targeting.movementDir(targets[1]);
+         currentTargetIndex = 1;
     }
     
     void Update()
     {
-        if(Vector3.Distance(transform.position, targets[previousTargetIndex]) >= DistanceToNextTarget) //checks for overshoot. if this condition is true, it's time to switch targets!
+        if (moveIfTouched == false)
         {
-             transform.position = targets[currentTargetIndex]; // snap to target position in case of overshoot
-             previousTargetIndex = currentTargetIndex;
-             if(currentTargetIndex == targets.Length) currentTargetIndex = 0; // if at the end of target array, loop back to 0
-             else currentTargetIndex++; //otherwise, increment the index by 1
-             DistanceToNextTarget = Vector3.Distance(targets[previousTargetIndex], targets[currentTargetIndex]);
-             dir = targeting.movementDir(targets[currentTargetIndex]);
+            if (Vector3.Distance(transform.position, targets[previousTargetIndex]) >= DistanceToNextTarget) //checks for overshoot. if this condition is true, it's time to switch targets!
+            {
+                transform.position = targets[currentTargetIndex]; // snap to target position in case of overshoot
+                previousTargetIndex = currentTargetIndex;
+                if (currentTargetIndex == targets.Length - 1) currentTargetIndex = 0; // if at the end of target array, loop back to 0
+                else currentTargetIndex++; //otherwise, increment the index by 1
+                DistanceToNextTarget = Vector3.Distance(targets[previousTargetIndex], targets[currentTargetIndex]);
+                dir = targeting.movementDir(targets[currentTargetIndex]);
+            }
+            transform.Translate(dir * speed * Time.deltaTime);
         }
-        transform.Translate(dir * speed * Time.deltaTime);
     }
-    
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (moveIfTouched == true)
+        {
+            if (Vector3.Distance(transform.position, targets[previousTargetIndex]) >= DistanceToNextTarget) //checks for overshoot. if this condition is true, it's time to switch targets!
+            {
+                transform.position = targets[currentTargetIndex]; // snap to target position in case of overshoot
+                previousTargetIndex = currentTargetIndex;
+                if (currentTargetIndex == targets.Length - 1) currentTargetIndex = 0; // if at the end of target array, loop back to 0
+                else currentTargetIndex++; //otherwise, increment the index by 1
+                DistanceToNextTarget = Vector3.Distance(targets[previousTargetIndex], targets[currentTargetIndex]);
+                dir = targeting.movementDir(targets[currentTargetIndex]);
+            }
+            transform.Translate(dir * speed * Time.deltaTime);
+        }
+    }
 }
