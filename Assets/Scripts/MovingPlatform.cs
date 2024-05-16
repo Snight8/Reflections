8c08692c96f444e56351d50d46561a9f270096fb
Chesyon <55322011+Chesyon@users.noreply.github.com>
2024-05-16 12:46:53 -0400

    Update MovingPlatform.cs
    I will be very surprised if this works

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -11,7 +11,9 @@ public class MovingPlatform : MonoBehaviour
     public int currentTargetIndex;
     public Vector3[] targets;
     float DistanceToNextTarget;
-    public bool moveIfTouched;
+    public bool running = true;
+    public bool touchRequired;
+    List<Collider> touchingObjects
 
     void Start() //do not use void Awake here!! Awake is called on load, and it's possible that mirrored platforms won't load on the same frame, which will desync them!
     {
@@ -23,7 +25,9 @@ public class MovingPlatform : MonoBehaviour
     
     void Update()
     {
-        if (moveIfTouched == false)
+        if (touchingObjects.Count == 0 && touchRequired) running = false;
+        else running = true;
+        if (running)
         {
             if (Vector3.Distance(transform.position, targets[previousTargetIndex]) >= DistanceToNextTarget) //checks for overshoot. if this condition is true, it's time to switch targets!
             {
@@ -35,23 +39,33 @@ public class MovingPlatform : MonoBehaviour
                 dir = targeting.movementDir(targets[currentTargetIndex]);
             }
             transform.Translate(dir * speed * Time.deltaTime);
+            touchingObjects.ForEach(MoveTouchingObject());
         }
     }
-
-    private void OnTriggerStay(Collider other)
+    
+    public void MoveTouchingObject(Collider touchingObject)
     {
-        if (moveIfTouched == true)
-        {
-            if (Vector3.Distance(transform.position, targets[previousTargetIndex]) >= DistanceToNextTarget) //checks for overshoot. if this condition is true, it's time to switch targets!
-            {
-                transform.position = targets[currentTargetIndex]; // snap to target position in case of overshoot
-                previousTargetIndex = currentTargetIndex;
-                if (currentTargetIndex == targets.Length - 1) currentTargetIndex = 0; // if at the end of target array, loop back to 0
-                else currentTargetIndex++; //otherwise, increment the index by 1
-                DistanceToNextTarget = Vector3.Distance(targets[previousTargetIndex], targets[currentTargetIndex]);
-                dir = targeting.movementDir(targets[currentTargetIndex]);
-            }
-            transform.Translate(dir * speed * Time.deltaTime);
-        }
+        touchingObject.transform.Translate(dir * speed * Time.deltaTime);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+    	//if the object is not already in the list
+    	if(!TriggerList.Contains(other))
+    	{
+    		//add the object to the list
+    		TriggerList.Add(Other);
+    	}
+    }
+    
+    //called when something exits the trigger
+    void OnTriggerExit(Collider other)
+    {
+    	//if the object is in the list
+    	if(TriggerList.Contains(other))
+    	{
+    		//remove it from the list
+    		TriggerList.Remove(Other);
+    	}
     }
 }
