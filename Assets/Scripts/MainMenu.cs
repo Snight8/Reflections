58b144df34dbe259bdf178ec42423197a502d13d
Chesyon <55322011+Chesyon@users.noreply.github.com>
2024-06-04 19:23:10 -0400

    menu stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
+    public Animator animator;
     public void QuitButton()
     {
         Application.Quit();
     }
     public void LevelSelectButton()
     {
-
+        animator.SetInteger("CurrentMenu", 1);
     }
     public void OptionsButton()
     {
-
+        animator.SetInteger("CurrentMenu", 2);
     }
     public void CreditsButton()
     {
-
+        animator.SetInteger("CurrentMenu", 3);
+    }
+    public void MainMenuButton()
+    {
+        animator.SetInteger("CurrentMenu", 0);
     }
 }
