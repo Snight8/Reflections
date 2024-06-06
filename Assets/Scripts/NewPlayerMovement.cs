9de92056421495e0049b20114de078ebdf133dcd
Chesyon <55322011+Chesyon@users.noreply.github.com>
2024-06-05 20:25:37 -0400

    animations??

--- Assets/Scripts/NewPlayerMovement.cs
+++ Assets/Scripts/NewPlayerMovement.cs
@@ -21,6 +21,8 @@ public class NewPlayerMovement : MonoBehaviour
 
     public AudioSource jumpSound;
 
+    public Animator animator;
+
     // Update is called once per frame
     void Update()
     {
@@ -32,11 +34,13 @@ public class NewPlayerMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
+            animator.SetBool("HasInput", true);
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             controller.Move(direction * speed * Time.deltaTime);
         }
+        else animator.SetBool("HasInput", false);
 
         // gravity code!
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -6,23 +6,40 @@ public class PressurePlate : MonoBehaviour
     public AudioSource UnclickSound;
     public AudioSource ClickSound;
     public GameObject thisWillGoAway; //this is just any object that we want to appear/disappear if you have something on the plate
+    bool pressed;
+    bool pressedLastFrame;
+    Vector3 initialPos;
 
-    void OnTriggerEnter()
+    void Awake()
     {
-        thisWillGoAway.SetActive(false);
-        ClickSound.Play();
-        transform.Translate(new Vector3(0, -0.15f, 0));
+        initialPos = transform.position;
+        pressed = false;
+        pressedLastFrame = false;
     }
 
-    /*void OnTriggerStay()
+    void OnTriggerStay()
     {
-        thisWillGoAway.SetActive(false);
-    }*/
+        pressed = true;
+    }
 
     void OnTriggerExit()
     {
-        thisWillGoAway.SetActive(true);
-        UnclickSound.Play();
-        transform.Translate(new Vector3(0, 0.15f, 0));
+        pressed = false;
+    }
+
+    void Update()
+    {
+        if (pressed)
+        {
+            transform.position = initialPos + new Vector3(0, -0.15f, 0);
+            if(!pressedLastFrame) ClickSound.Play();
+        }
+        else
+        {
+            transform.position = initialPos;
+            if(pressedLastFrame) UnclickSound.Play() ;
+        }
+        thisWillGoAway.SetActive(!pressed);
+        pressedLastFrame = pressed;
     }
 }
